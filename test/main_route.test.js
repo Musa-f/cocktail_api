6184fe93a4642fa9c25a97ce851f2bd4e84f0b65
musa-f <musaevafatima77@gmail.com>
2024-06-07 14:37:04 +0200

    Config files

---
+++ test/main_route.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+const app = require('../server')
+
+describe('MAIN ROUTER', () => {
+
+    describe('TRY GET', () => {
+        it('Sould return 200', async() => {
+            const response = await request(app).get('/')
+            expect(response.status).toBe(200)
+        })
+
+        it('Sould return 501', async() => {
+            const response = await request(app).get('/fdbhcdgvsjrvbuik')
+            expect(response.status).toBe(501)
+        })
+    })
+
+    describe('TRY PATCH', () => {
+        it('Sould return 501', async() => {
+            const response = await request(app).patch('/')
+            expect(response.status).toBe(501)
+        })
+
+        it('Sould return 501', async() => {
+            const response = await request(app).patch('/fdbhcdgvsjrvbuik')
+            expect(response.status).toBe(501)
+        })
+    })
+})
